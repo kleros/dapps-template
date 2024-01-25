99517540e3a3a014f46cbe4152e3b44523b50d45
jaybuidl <jb@kleros.io>
2024-01-25 15:30:58 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# {{project_name}}
+
+Setup:
+
+```bash
+yarn
+```
+
+The frontend is located in the `web` folder
+
+```bash
+cd web
+yarn && yarn start
+```
+
---
+++ web/src/app.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Navigate, Route } from "react-router-dom";
+import { SentryRoutes } from "./utils/sentry";
+import "react-loading-skeleton/dist/skeleton.css";
+import "react-toastify/dist/ReactToastify.css";
+import Web3Provider from "context/Web3Provider";
+import IsListProvider from "context/IsListProvider";
+import QueryClientProvider from "context/QueryClientProvider";
+import StyledComponentsProvider from "context/StyledComponentsProvider";
+import RefetchOnBlock from "context/RefetchOnBlock";
+import Layout from "layout/index";
+import NewTransaction from "./pages/NewTransaction";
+import MyTransactions from "./pages/MyTransactions";
+import { NewTransactionProvider } from "./context/NewTransactionContext";
+
+const App: React.FC = () => {
+  return (
+    <StyledComponentsProvider>
+      <QueryClientProvider>
+        <RefetchOnBlock />
+        <Web3Provider>
+          <IsListProvider>
+            <NewTransactionProvider>
+              <SentryRoutes>
+                <Route path="/" element={<Layout />}>
+                  <Route index element={<Navigate to="newTransaction" replace />} />
+                  <Route path="newTransaction/*" element={<NewTransaction />} />
+                  <Route path="myTransactions/*" element={<MyTransactions />} />
+                  <Route path="*" element={<h1>404 not found</h1>} />
+                </Route>
+              </SentryRoutes>
+            </NewTransactionProvider>
+          </IsListProvider>
+        </Web3Provider>
+      </QueryClientProvider>
+    </StyledComponentsProvider>
+  );
+};
+
+export default App;
